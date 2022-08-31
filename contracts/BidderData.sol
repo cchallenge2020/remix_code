bbb0c0e3f40e392792da806abc698ceec903f7f3
Remix Workspace <>
2022-08-31 19:32:01 +0530

    added some contracts

--- contracts/BidderData.sol
+++ contracts/BidderData.sol
@@ -2,8 +2,19 @@ pragma solidity ^0.4.0;
 
 contract Bidder {
 
-    string public name;
+    string public name = "buffalo";
     uint public bidAmount = 20000;
     bool public eligible;
     uint constant minBid = 1000;
-}
+
+    function setName(string n) public {
+        name = n;
+    }
+    function setBidAmt(uint amt) public {
+        bidAmount = amt;
+    }
+    function determineEligibility() public {
+        if(bidAmount>=minBid) eligible = true;
+        else eligible = false;
+    }
+ }
